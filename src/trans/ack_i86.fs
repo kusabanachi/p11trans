0c661c2c01e75b4f6b61694687bd5c763d01c3ee
kusabanachi <currysick@gmail.com>
2014-11-24 00:04:55 +0900

    Add translation of 'add type' code

--- src/trans/ack_i86.fs
+++ src/trans/ack_i86.fs
@@ -4,14 +4,32 @@ module Ack_i86_trans =
 
     open StatementType
     open Instruction
+    open Expres
 
     let ack_i86_asm pdp11as =
+        let singleOp code addr =
+            let immVal num = Addres.Imm (Expr_Oct (int16 num))
+            match code with
+            | "clr" -> addType "and" addr (immVal 0)
+            | "com" -> addType "xor" addr (immVal 0xffff)
+            | "adc" -> addType "adc" addr (immVal 0)
+            | "sbc" -> addType "sbb" addr (immVal 0)
+            | "ror" -> addType "rcr" addr (immVal 1)
+            | "rol" -> addType "rcl" addr (immVal 1)
+            | "asr" -> addType "sar" addr (immVal 1)
+            | "asl" -> addType "sal" addr (immVal 1)
+            | _ -> ""
+
         let doubleOp code dest src =
             match code with
-            | "mov" -> movType code dest src
+            | "mov" -> movType "mov" dest src
+            | "bis" -> addType "or" dest src
+            | "add" -> addType "add" dest src
+            | "sub" -> addType "sub" dest src
             | _ -> ""
 
         let transStatement = function
+            | SingleOp (code, addr)      -> singleOp code addr
             | DoubleOp (code, src, dest) -> doubleOp code dest src
             | _ -> ""
 
--- src/trans/instruction.fs
+++ src/trans/instruction.fs
@@ -58,3 +58,33 @@ module Instruction =
             binaryCalc code dest src
 
 
+
+    let addType code dest src =
+        let dest = i86Addr dest
+        let src = i86Addr src
+
+        if src.isMemory && dest.isMemory then
+            if dest.isAccessible then
+                let code1, src = moveVal utilReg src
+                let code2 = binaryCalc code dest src
+                code1 +!!+ code2
+            else
+                let saveReg = findFreeReg src dest
+                let code1       = storeRegVal saveReg
+                let code2, src  = moveVal saveReg src
+                let code3, dest = moveRef utilReg dest
+                let code4       = binaryCalc code dest src
+                let code5       = restoreRegVal saveReg
+                code1 +!!+ code2 +!!+ code3 +!!+ code4 +!!+ code5
+        elif not dest.isAccessible then
+            let code1, dest = moveRef utilReg dest
+            let code2 = binaryCalc code dest src
+            code1 +!!+ code2
+        elif not src.isAccessible then
+            let code1, src = moveRef utilReg src
+            let code2 = binaryCalc code dest src
+            code1 +!!+ code2
+        else
+            binaryCalc code dest src
+
+
